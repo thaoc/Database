eee3a0443dc0582f54eae18823df0ddff7bb17a5
KooikerH <kooikerh@csp.edu>
2018-11-13 15:34:02 -0600

    Add files via upload

--- insert_facility.sql
+++ insert_facility.sql
@@ -1,5 +1,10 @@
 insert into Facility (
-	Facility.Facility_Name
-) values (
-	"Scotland Yard"
-)
+	Facility.Facility_Name,
+    Facility.Facility_Phone, 
+    Facility.Facility_Street,
+    Facility.Facility_City, 
+    Facility.Facility_State, 
+    Facility.Facility_Zip
+) values 
+	("Rochester", "507-292-1700", "1449 4th Ave SE","Rochester", "MN",  55904),
+    ("Golden Valley", "763-545-6558", "5730 Olson Memorial Hwy", "Minneapolis", "MN", 55422);
